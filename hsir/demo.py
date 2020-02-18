a47858e19e2d2f1707354d4678cd9ecfe8f42fea
Wenjie Zheng <work@zhengwenjie.net>
2020-02-18 19:25:12 +0800

    demo

---
+++ hsir/demo.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  \\[
+#  \begin{aligned}
+#  \frac{dS}{dt} &= -\frac{\beta SI}{N} \\
+#  \frac{dI}{dt} &= \frac{\beta SI}{N} - \gamma I\\
+#  \frac{dR}{dt} &= \gamma I 
+#  \end{aligned}
+#  \\]
+
+# In[1]:
+
+
+import numpy as np
+from model import *
+from law import *
+from infer import InferSIR
+
+
+# In[2]:
+
+
+T = 100
+city = Region(9990, 10, 0)
+epidemic = Epidemic(city, SIR(3, 1, 0.1), T)
+np.random.seed(0)
+sample = Sample(epidemic, np.arange(T//10, T, T//10), 10*np.ones(9, dtype=int), Poi)
+sample.positive = np.array([0, 1, 2, 6, 1, 0, 0, 1, 0])
+sample.plot(epidemic)
+
+
+# In[3]:
+
+
+learner = InferSIR(algo="mcmc")
+learner.fit(city, sample)
+print(learner)
+
